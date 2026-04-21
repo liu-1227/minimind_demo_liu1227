f1a0cccbd0944db6b995a6b7bf261f97b115d534
liurupan <3454696866@qq.com>
2026-04-21 22:29:14 +0800

    config配置（带部分注释） RMSNorm归一化函数

--- model/model.py
+++ model/model.py
@@ -81,3 +81,22 @@ class MokioMindConfig(PretrainedConfig):
             if self.inference_rope_scaling
             else None
         )
+
+
+import torch
+import torch.nn as nn
+
+#继承nn.Module类
+class RMSNorm(nn.Module):
+#__init__初始化
+    def __init__(self,dim:int,eps:float=1e-5):
+        super().__init__()
+        self.dim = dim
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(dim))
+#_norm
+    def norm(self,x):
+        return x*torch.rsqrt(x.pow(2).mean(-1,keepdim = True)+self.eps)
+#forward
+    def forward(self, x):
+        return self.weight * self.norm(x.float()).type_as(x)
