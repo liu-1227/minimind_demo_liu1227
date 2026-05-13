d28e9d7ae3ccfde1507685e8fddd124b7f173aa0
liurupan <3454696866@qq.com>
2026-05-13 11:31:26 +0800

    拼接

--- model/model.py
+++ model/model.py
@@ -349,7 +349,43 @@ class FeedForward(nn.Module):
 
 
 
+class MokioMindBlock(nn.Module):
+    def __init__(self,layer_id:int,config:MokioMindConfig):
+        super().__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.hidden_size = config.hidden_size
+        self.head_dim = self.hidden_size // self.num_attention_heads
+        self.self_attn = Attention(config) #多头注意力模块
 
+        self.layer_id = layer_id
+        self.input_layernorm = RMSNorm(config.hidden_size,eps=config.rms_norm_eps)
+        self.post_attention_layernorm = RMSNorm(config.hidden_size,eps=config.rms_norm_eps)
+        self.mlp = FeedForward(config) #FFN
 
-
+    def forward(
+        self,
+        hidden_states,
+        position_embeddings,
+        past_key_value = None,
+        use_cache = False,
+        attention_mask = False
+    ):
+        #保存残差，后续相加使用
+        residual = hidden_states
+        #返回新的hidden_states,kv的cache
+        hidden_states,present_key_value = self.self_attn(
+            self.input_layernorm(hidden_states),#pre_norm
+            position_embeddings,
+            past_key_value,
+            use_cache,
+            attention_mask,
+        )
+        #注意力输出 + 残差（第一个残差连接）
+        hidden_states = residual + hidden_states
+        #前馈子层 （第二个残差连接）
+        hidden_states = hidden_states + self.mlp(
+            self.post_attention_layernorm(hidden_states)
+        )
+        #返回当前层的输出和新KV cache
+        return hidden_states,present_key_value
 
